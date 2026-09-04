d183273a4ac06b755836985bc02e3a8f03ec25ba
Sarvesh2708 <91408204+Sarvesh2708@users.noreply.github.com>
2026-09-04 16:02:25 -0700

    Add solution for Check Bipartite Graph (C++) [Code360 Sync]

--- coding-ninjas-solutions/Uncategorized/Check Bipartite Graph/solution.cpp
+++ coding-ninjas-solutions/Uncategorized/Check Bipartite Graph/solution.cpp
@@ -1,36 +1,40 @@
-#include <queue>
+bool dfs(int node, int col, vector<int>& visited, vector<int> adj[]) {
+    visited[node] = col;
 
-bool isGraphBirpatite(vector<vector<int>> &edges) {
-	// Write your code here.
-	queue<int> q;
-	int n = edges.size();
-	vector<int> adj[n];
-	for (int i = 0; i < n; i++) {
+    for (auto it : adj[node]) {
+        if (visited[it] == -1) {
+            if (dfs(it, !col, visited, adj) == false)
+                return false;
+        }
+        else if (visited[it] == col) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+bool isGraphBirpatite(vector<vector<int>>& edges) {
+    int n = edges.size();
+
+    vector<int> adj[n];
+
+    for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             if (edges[i][j] == 1) {
                 adj[i].push_back(j);
             }
         }
     }
-	vector<int> visited(n, -1);
-	q.push(0);
-	visited[0]=0;
-	while(!q.empty()){
-		int node = q.front();
-		q.pop();
-		for(auto it:adj[node]){
-			if(visited[it]==-1){
-				visited[it] = !visited[node];
-				q.push(it);
-			}
-			else if(visited[it]==visited[node]){
-				return false;
-			}
-		}
-	}
-	return true;
 
+    vector<int> visited(n, -1);
 
+    for (int i = 0; i < n; i++) {
+        if (visited[i] == -1) {
+            if (dfs(i, 0, visited, adj) == false)
+                return false;
+        }
+    }
 
-	 
+    return true;
 }
