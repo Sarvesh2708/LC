d07b295176d7a07ef73b10c9379c562a260bc32f
Sarvesh2708 <91408204+Sarvesh2708@users.noreply.github.com>
2026-09-04 23:21:50 -0700

    Add solution for Detect Cycle In A Directed Graph (C++) [Code360 Sync]

---
+++ coding-ninjas-solutions/Uncategorized/Detect Cycle In A Directed Graph/solution.cpp
@@ -0,0 +1,39 @@
+bool dfs(int node,vector<vector<int>> &adj,int visited[],int pathvisited[]){
+  visited[node]=1;
+  pathvisited[node]=1;
+  for(auto it:adj[node]){
+    if(!visited[it]){
+      if(dfs(it,adj,visited,pathvisited)==true) return true;
+    }
+  else if(pathvisited[it]){
+    return true;
+  }
+  }
+  pathvisited[node]=0;
+  return false;
+}
+
+
+int detectCycleInDirectedGraph(int n, vector < pair < int, int >> & edges) {
+  // Write your code here.
+  vector<vector<int>> adj(n);
+
+  for (auto edge : edges) {
+      int u = edge.first;
+      int v = edge.second;
+
+      adj[u].push_back(v);
+  }
+  vector<int> visited(n, 0);
+  vector<int> visited(n, 0);
+  vector<int> pathvisited(n, 0);
+
+
+  for(int i=0;i<n;i++){
+    if(!visited[i]){
+      if(dfs(i,adj,visited,pathvisited)==true) return 1;
+    }
+  }
+  return 0;
+
+}
