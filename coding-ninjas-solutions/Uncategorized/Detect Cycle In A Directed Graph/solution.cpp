2ef246f1f8668399eacc1de4b49c0729e18e98b4
Sarvesh2708 <91408204+Sarvesh2708@users.noreply.github.com>
2026-09-05 22:11:39 -0700

    Add solution for Detect Cycle In A Directed Graph (C++) [Code360 Sync]

--- coding-ninjas-solutions/Uncategorized/Detect Cycle In A Directed Graph/solution.cpp
+++ coding-ninjas-solutions/Uncategorized/Detect Cycle In A Directed Graph/solution.cpp
@@ -1,39 +1,57 @@
-bool dfs(int node,vector<vector<int>> &adj,vector<int> &visited,vector<int> &pathvisited){
-  visited[node]=1;
-  pathvisited[node]=1;
-  for(auto it:adj[node]){
-    if(!visited[it]){
-      if(dfs(it,adj,visited,pathvisited)==true) return true;
+#include <queue>
+#include <vector>
+using namespace std;
+
+int detectCycleInDirectedGraph(int n, vector<pair<int, int>> &edges) {
+    
+    vector<int> indegree(n, 0);
+    vector<vector<int>> adj(n);
+    queue<int> q;
+
+    // Create adjacency list
+    for (auto edge : edges) {
+        int u = edge.first;
+        int v = edge.second;
+
+        adj[u].push_back(v);
     }
-  else if(pathvisited[it]){
+
+    // Calculate indegree of every node
+    for (int i = 0; i < n; i++) {
+        for (auto it : adj[i]) {
+            indegree[it]++;
+        }
+    }
+
+    // Push all nodes with indegree 0
+    for (int i = 0; i < n; i++) {
+        if (indegree[i] == 0) {
+            q.push(i);
+        }
+    }
+
+    int cnt = 0;
+
+    // Kahn's Algorithm
+    while (!q.empty()) {
+        int node = q.front();
+        q.pop();
+
+        cnt++;
+
+        for (auto it : adj[node]) {
+            indegree[it]--;
+
+            if (indegree[it] == 0) {
+                q.push(it);
+            }
+        }
+    }
+
+    // If all nodes are processed, there is no cycle
+    if (cnt == n)
+        return false;
+
+    // Otherwise, cycle exists
     return true;
-  }
-  }
-  pathvisited[node]=0;
-  return false;
-}
-
-
-int detectCycleInDirectedGraph(int n, vector < pair < int, int >> & edges) {
-  // Write your code here.
-  vector<vector<int>> adj(n + 1);
-
-
-  for (auto edge : edges) {
-      int u = edge.first;
-      int v = edge.second;
-
-      adj[u].push_back(v);
-  }
-  vector<int> visited(n+1, 0);
-  vector<int> pathvisited(n+1, 0);
-
-
-  for(int i=0;i<n;i++){
-    if(!visited[i]){
-      if(dfs(i,adj,visited,pathvisited)==true) return 1;
-    }
-  }
-  return 0;
-
 }
